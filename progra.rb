14809f8326a0deac6ab6dccb4ac9b12ebef81f4d
JnyAlx100 <38715956+JnyAlx100@users.noreply.github.com>
2018-10-28 23:16:27 -0600

    Update progra.rb

--- progra.rb
+++ progra.rb
@@ -91,3 +91,114 @@ def insertar_cola(cola, elemento)
         end
     end while a != nil
 end
+
+def registro_libros(cola_autores)
+    puts 'Ingrese el ISBN:'
+    isbn = gets.chomp.to_i
+
+    a = buscar_libro(cola_autores, isbn) 
+    if a != nil
+        a[:existencias] += 1
+        puts 'Se ha encontrado el libro y se han incrementado sus existencias'
+        return
+    else
+        puts 'Ingrese nombre del autor:'
+        autor = gets.chomp
+        if buscar(cola_autores,autor,:nombre) == nil && llena?(cola_autores) == true
+            puts 'Este autor no esta registrado y su cola ya esta llena'
+            return
+        end
+    end
+
+    puts 'Ingrese nombre del libro:'
+    nombre = gets.chomp
+    puts 'Ingrese el precio:'
+    precio = gets.chomp.to_i
+    puts 'Libro exitosamente registrado'
+
+    a = cola_autores[:tope]
+    
+    elementolibro = {
+        nombre: nombre,
+        existencias: 1,
+        precio: precio,
+        isbn: isbn,
+        autor: autor,
+        siguiente: nil
+    }
+    pilaautor = {
+        nombre: autor,
+        tope: elementolibro,
+        siguiente: nil
+    }
+
+    begin
+        if a == nil
+            cola_autores[:tope] = pilaautor
+            cola_autores[:fondo] = pilaautor
+            return
+        elsif a[:nombre] == autor
+            break
+        elsif a[:siguiente] == nil
+            a[:siguiente] = pilaautor
+            cola_autores[:fondo] = pilaautor
+            return
+        else
+            a = a[:siguiente]
+        end
+    end while a != nil
+
+    b = a[:tope]
+    begin
+        if b == nil
+            a[:tope] = elementolibro
+            break
+        elsif b[:siguiente] == nil
+            elementolibro[:siguiente] = a[:tope]
+            a[:tope] = elementolibro
+            break
+        else
+            b = b[:siguiente]
+        end
+    end while b != nil
+end
+
+def registro_autores(cola_autores)
+    if llena?(cola_autores) == true
+        puts 'Su cola de autores ya esta llena'
+    else
+        puts 'Ingrese nombre del autor'
+        nombre = gets.chomp
+
+        a = buscar(cola_autores,nombre,:nombre)
+        if a != nil
+            puts 'Este autor ya existe'            
+            return
+        end
+
+        pilaautor = {
+            nombre: nombre,
+            tope: nil,
+            siguiente: nil
+        }
+
+        insertar_cola(cola_autores, pilaautor)
+        puts 'Autor exitosamente registrado'
+    end
+end
+
+def listado_libros(cola_autores)
+    a = cola_autores[:tope]
+    rows = []
+    while a != nil
+        b = a[:tope]
+        while b != nil
+            rows << [b[:isbn], b[:nombre], b[:precio], b[:existencias], a[:nombre]]
+            b = b[:siguiente]
+        end
+        a = a[:siguiente]
+    end
+    table = Terminal::Table.new :rows => rows
+    table.headings = ['ISBN', 'Nombre', 'Precio','Existencias', 'Autor']
+    puts table
+end
