2021aabeb92f55a9c65c44086d0f15c0a9f6f2af
Josue Mendez <jossugames@gmail.com>
2018-10-28 23:21:46 -0600

    Solo Faltan las metodos de Douglas

--- progra.rb
+++ progra.rb
@@ -202,3 +202,169 @@ def listado_libros(cola_autores)
     table.headings = ['ISBN', 'Nombre', 'Precio','Existencias', 'Autor']
     puts table
 end
+
+def listado_autores(cola_autores)
+    if vacia?(cola_autores) == true
+        puts 'La cola de autores esta vacia'
+        return
+    end
+    a = cola_autores[:tope]
+    rows = []
+    while a != nil
+        b = a[:tope]
+        existencias = 0
+        while b != nil
+            existencias += 1
+            b = b[:siguiente]
+        end
+        rows << [ a[:nombre], existencias]
+        a = a[:siguiente]
+    end
+    table = Terminal::Table.new :rows => rows
+    table.headings = ['Autor', 'Existencias']
+    puts table
+end
+
+def ingreso_buscar_libro(cola_autores)
+    rows = []
+    if vacia?(cola_autores)
+        puts 'Su base de datos esta vacia'
+    else
+        puts 'Ingrese el ISBN del libro buscado:'
+        isbn = gets.chomp.to_i
+        a = buscar_libro(cola_autores, isbn)
+        if a == nil
+            puts 'Este libro no esta registrado'
+        else
+        rows << [a[:nombre], a[:existencias], isbn, a[:precio],a[:autor]]
+        table = Terminal::Table.new :rows => rows 
+        table.headings = ['Nombre', 'Existencia', 'ISBN', 'Precio', 'Autor']
+        puts table
+        end
+    end
+end
+
+def ingreso_buscar_autor(cola_autores)
+    if vacia?(cola_autores) == true
+        puts 'Su base de datos esta vacia'
+    else
+        puts 'Ingrese nombre del autor: '
+        nombreautor = gets.chomp
+        a = buscar(cola_autores, nombreautor, :nombre)
+        if a == nil
+            puts 'Este autor no existe'
+            return
+        end
+        if vacia?(a) == true
+            puts 'Este autor aun no tiene libros'
+            return
+        end
+        elemento = {}
+        aux = cola_autores[:tope]
+        rows = []
+    loop do
+        if  nombreautor == aux[:nombre]
+            elemento = aux
+            break
+        elsif aux[:siguiente] == nil
+            break
+        else
+            aux = aux[:siguiente]
+        end
+    end
+    puts "Autor: #{elemento[:nombre]}"
+    elemento = elemento[:tope]
+    
+    loop do
+        rows << [elemento[:nombre], elemento[:existencias]]
+        elemento = elemento[:siguiente]
+        if elemento == nil
+            break
+        end
+    end
+    table = Terminal::Table.new :rows => rows 
+    table.headings = ['Libros', 'Existencias']
+    puts table
+    end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+begin
+    limpiar
+    puts 'Bienvenido a la base de datos "Ruby Bookstore"'
+    puts 'Administración de libros'
+    puts ' 1. Registro de nuevos libros'
+    puts ' 2. Registro de autores'
+    puts ' 3. Listado de libros'
+    puts ' 4. Listado de autores'
+    puts ' 5. Buscar libro'
+    puts ' 6. Buscar autor'
+    puts 'Control de ventas'
+    puts ' 7. Registrar una venta'
+    puts ' 8. Buscar una venta'
+    puts ' 9. Listado de ventas'
+    puts '10. Salir'
+
+    n = gets.chomp.to_i
+
+    if n == 1
+        limpiar
+        registro_libros(cola_autores)
+        continuar
+    elsif n == 2
+        limpiar
+        registro_autores(cola_autores)
+        continuar
+    elsif n == 3
+        limpiar
+        listado_libros(cola_autores)
+        continuar
+    elsif n == 4
+        limpiar
+        listado_autores(cola_autores)
+        continuar
+    elsif n == 5
+        limpiar
+        ingreso_buscar_libro(cola_autores)
+        continuar
+    elsif n == 6
+        limpiar
+        ingreso_buscar_autor(cola_autores)
+        continuar
+    elsif n == 7
+        limpiar
+        registro_ventas(cola_autores, cola_ventas)
+        continuar
+    elsif n == 8
+        limpiar
+        ingreso_buscar_venta(cola_ventas)
+        continuar
+    elsif n == 9
+        limpiar
+        listado_ventas(cola_ventas)
+        continuar
+    elsif n == 10
+        limpiar
+        puts 'Que tenga un buen dia'
+    else
+        puts 'Opcion invalida'
+        continuar
+    end
+end while n != 10
